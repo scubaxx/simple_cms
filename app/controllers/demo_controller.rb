44131bd744006966010b90bd62cf48f6f75d0a0c
Mauricio F Martinez <mauriciof.martinez@gmail.com>
2016-03-06 02:24:02 -0800

    Adding comments

--- app/controllers/demo_controller.rb
+++ app/controllers/demo_controller.rb
@@ -6,10 +6,19 @@ class DemoController < ApplicationController
   	end
 
 	def hello
-		# render('index')
 		@array = [1,2,3,4,5]
+		# Defyning the @instance_variables in my controller
 		@id = params['id'].to_i
+		# I wanna read the parameter id, so here I am calling
+		# the string 'id' from the view index.html.erb
+		# using an @instance_variable @id
+		# The method "to_i" converts a string to an integer
 		@page = params[:page].to_i
+		# I wanna read the parameter page, I'm using the symbol
+		# this time :page ,from index.html.erb
+		# using @instance_variable @page
+		# The method "to_i" converts a string to an integer
+
 	end	
 
 	def other_hello
