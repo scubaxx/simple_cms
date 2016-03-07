eb929e39859b0a469c8d264f99b9b0ecf966cd98
Mauricio F Martinez <mauriciof.martinez@gmail.com>
2016-03-07 04:00:02 -0800

    fixed db

--- db/schema.rb
+++ db/schema.rb
@@ -11,18 +11,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151127044223) do
-
-  create_table "admin_users", force: :cascade do |t|
-    t.string   "first_name",      limit: 25
-    t.string   "last_name",       limit: 50
-    t.string   "email",           limit: 100, default: "", null: false
-    t.string   "username",        limit: 25
-    t.string   "hashed_password", limit: 40
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  add_index "admin_users", ["username"], name: "index_admin_users_on_username", using: :btree
+ActiveRecord::Schema.define(version: 0) do
 
 end
