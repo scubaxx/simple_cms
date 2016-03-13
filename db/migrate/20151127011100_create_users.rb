c9affee8f6a61d3f69ea0f078aa60d78af30bcd1
Mauricio F Martinez <mauriciof.martinez@gmail.com>
2016-03-13 00:36:09 -0800

    it is all good

--- db/migrate/20151127011100_create_users.rb
+++ db/migrate/20151127011100_create_users.rb
@@ -2,18 +2,21 @@ class CreateUsers < ActiveRecord::Migration
 
   def up
     create_table :users do |t|
+      # "create_table" is the migration method 
     	t.string "first_name", :limit => 25
     	t.string "last_name", :limit => 50
     	t.string "email", :default => "", :null => false
     	t.string "password", :limit => 40
-
-
+      # t.string "first_name" is the short format
+      # t.column "first_name", :string is the long format
+      # options are :limit, :default, :null
       t.timestamps
     end
   end
 
   def down
   	drop_table :users
+    # "drop_table" is the migration method
   end
   	
 end
