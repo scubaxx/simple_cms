d9ace43da4dc5ffb029f16f50066a6b82c234a86
Mauricio F Martinez <mauriciof.martinez@gmail.com>
2016-04-28 01:14:43 -0700

    Changes to allow flow thru the join -modified models admin_user.rb & section.rb

--- app/models/admin_user.rb
+++ app/models/admin_user.rb
@@ -5,4 +5,6 @@ class AdminUser < ActiveRecord::Base
 	# Tells this class that it has a different table name
 	has_and_belongs_to_many :pages
 	has_many :section_edits
+	has_many :sections, :through => :section_edits
+	
 end
