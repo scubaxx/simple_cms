4e133cb412d19302025261e35ed94392ef35ad35
Mauricio F Martinez <mauriciof.martinez@gmail.com>
2016-04-30 23:02:09 -0700

    Implemented subjects/index In the controller I added an instance variable @subjects that pulled data from the model subject.rb ,my instance variable was applied a method-scope-lambda called sorted, then in views/subject/index I added HTML I inserted ruby code and conditionals in the form of ternary operator

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
 
+  # get 'subjects/index'
+  # Im commenting this out because I have my router
+  # get 'subjects/show'
+  # get 'subjects/new'
+  # get 'subjects/edit'
+  # get 'subjects/delete'
+
   root "demo#index"
 
   #get 'demo/index'
